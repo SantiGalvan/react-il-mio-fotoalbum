e158d1b42abf84140c1801d5cb7d2805e3e4c0e6
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-26 10:53:34 +0200

    added crud routes

--- backend/routers/photos.js
+++ backend/routers/photos.js
@@ -1,4 +1,20 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router(); ù
+const photosController = require("../controllers/photos.js");
+
+// Rotta Store
+router.post('/', photosController.store);
+
+// Rotta Index
+router.get('/', photosController.index);
+
+// Rotta Show
+router.get('/:slug', photosController.show);
+
+// Rotta update
+router.put('/:slug', photosController.update);
+
+// Rotta Delete
+router.delete('/:slug', photosController.destroy);
 
 module.exports = router;
