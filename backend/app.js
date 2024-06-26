4d791f1437163ca481e281da42ed1b5005579812
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-26 15:49:48 +0200

    added categories routes

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const host = process.env.HOST || 'localhost';
 const cors = require("cors");
 const photosRouter = require("./routers/photos.js");
 const authRouter = require("./routers/auth.js");
+const categoriesRouter = require("./routers/categories.js");
 
 app.use(cors());
 
@@ -17,6 +18,9 @@ app.use('/auth', authRouter);
 
 // Photo
 app.use('/photos', photosRouter);
+
+// Category
+app.use('/categories', categoriesRouter);
 
 app.listen(port, host, () => {
     console.log(`Server attivo su http://${host}:${port}`);
