d6d53b2d058499339f7e5a25971340117afee970
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-26 17:43:49 +0200

    created generalValidation

---
+++ backend/validations/generalValidation.js
@@ -0,0 +1,36 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const validationSlug = {
+    slug: {
+        in: ["params"],
+        custom: {
+            options: async (value) => {
+                const slug = await prisma.photo.findUnique({ where: { slug: value } });
+
+                if (!slug) throw new Error(`Non esiste un post con slug: ${value}`);
+
+                return true;
+            }
+
+        }
+    }
+}
+
+const validationCategorySlug = {
+    id: {
+        in: ["params"],
+        custom: {
+            options: async (value) => {
+                const slug = await prisma.category.findUnique({ where: { slug: value } });
+
+                if (!slug) throw new Error(`Non esiste una categoria con slug:${value}`);
+
+                return true;
+            }
+
+        }
+    }
+}
+
+module.exports = { validationSlug, validationCategorySlug };
