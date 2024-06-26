49d3d2f884a62bc04cecb3039002a34cb4a20693
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-26 17:44:38 +0200

    added categories validations

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const categoriesController = require("../controllers/categories.js");
+const validator = require('../middlewares/validator.js');
+const { validationCategorySlug } = require("../validations/generalValidation.js");
+const { bodyData } = require("../validations/categories.js");
 
 // Rotta Store
-router.post('/', categoriesController.store);
+router.post('/', validator(bodyData), categoriesController.store);
 
 // Rotta Index
 router.get('/', categoriesController.index);
+
+// Validatore dello slug delle categorie
+router.use('/:slug', validator(validationCategorySlug));
 
 // Rotta Show
 router.get('/:slug', categoriesController.show);
 
 // Rotta Update
-router.put('/:slug', categoriesController.update);
+router.put('/:slug', validator(bodyData), categoriesController.update);
 
 // Rotta Delete
 router.delete('/:slug', categoriesController.destroy);
