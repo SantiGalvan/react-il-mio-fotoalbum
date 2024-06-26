6731fbaa65717f17500e69c1959ebcceff85004c
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-26 15:28:37 +0200

    added user login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -33,7 +33,33 @@ const register = async (req, res) => {
     }
 }
 
-const login = (req, res) => { }
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) throw new Error('Email errata');
+
+        const isPasswordValid = await comparePassword(password, user.password);
+        if (!isPasswordValid) throw new Error('Password errata');
+
+        const token = generateToken({
+            email: user.email,
+            name: user.name,
+            isAdmin: user.isAdmin,
+            isSuperAdmin: user.isSuperAdmin
+        });
+
+        delete user.id;
+        delete user.password;
+
+        res.status(200).json({ token, data: user });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+}
 
 const index = (req, res) => { }
 
