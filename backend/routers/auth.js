f8ddcf489dcf0360ec6f9f1cdf8e2df25e53476c
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-26 17:45:42 +0200

    added users validations

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.js");
+const validator = require("../middlewares/validator.js");
+const { registerBody, loginBody } = require("../validations/users.js");
 
 // Rotta Register
-router.post('/register', authController.register);
+router.post('/register', validator(registerBody), authController.register);
 
 // Rotta Login
-router.post('/login', authController.login);
+router.post('/login', validator(loginBody), authController.login);
 
 // Rotta Index
 router.get('/users', authController.index);
