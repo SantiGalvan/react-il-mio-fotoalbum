212a5d622132a7ed39d0b631110f2734062c0413
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-26 10:44:04 +0200

    added route photos

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,14 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost';
 const cors = require("cors");
+const photosRouter = require("./routers/photo.js");
 
 app.use(cors());
 
 app.use(express.json());
+
+// Photo
+app.use('/photos', photosRouter);
 
 app.listen(port, host, () => {
     console.log(`Server attivo su http://${host}:${port}`);
