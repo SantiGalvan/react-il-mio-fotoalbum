510f4ef48dd9706eeea42008b1d4d1adfe572e7b
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-26 15:01:10 +0200

    created routes auth

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,14 @@ const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost';
 const cors = require("cors");
 const photosRouter = require("./routers/photos.js");
+const authRouter = require("./routers/auth.js");
 
 app.use(cors());
 
 app.use(express.json());
+
+// Auth
+app.use('/auth', authRouter);
 
 // Photo
 app.use('/photos', photosRouter);
