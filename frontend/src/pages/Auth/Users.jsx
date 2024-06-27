2d52a018c4709c4802579013677430109c52b8aa
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-28 00:14:57 +0200

    created users page

---
+++ frontend/src/pages/Auth/Users.jsx
@@ -0,0 +1,9 @@
+const Users = () => {
+    return (
+        <section className="container">
+            <h1 className="text-center">Utenti</h1>
+        </section>
+    )
+}
+
+export default Users;
