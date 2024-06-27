0f33a99617786d7cda77d717ad40b557801d9a56
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 14:22:42 +0200

    added fetchPhotos

--- frontend/src/pages/PhotosPages/PhotosIndex.jsx
+++ frontend/src/pages/PhotosPages/PhotosIndex.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "../../utils/axiosClient.js";
+
 const PhotosIndex = () => {
+
+    const [photos, setPhotos] = useState();
+
+    const fetchPhotos = async () => {
+        const res = await axios.get('/photos');
+        const newPhotos = res.data.data;
+        setPhotos(newPhotos);
+    }
+
+    useEffect(() => {
+        fetchPhotos();
+    }, []);
+
     return (
-        <section>
-            <h1>Foto</h1>
+        <section className="container">
+            <h1 className="text-center">Foto</h1>
+            <div className="row">
+                {photos?.map(({ id, title }) => (
+                    <div key={`photo-${id}`} className="col-4">
+                        <p>{title}</p>
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
