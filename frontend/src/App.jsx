3c8704dc1a0dd3835e3b4299481212667a33016e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 12:55:02 +0200

    added home page with defaul layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,21 @@
-const App = () => { }
+import { Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Routes>
+
+      {/* Rotte pubbliche */}
+      <Route path="/" element={<DefaultLayout />} >
+
+        {/* Home */}
+        <Route index element={<Home />} />
+
+      </Route>
+
+    </Routes>
+  )
+}
 
 export default App;
