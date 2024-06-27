6a33bc4638ee504bbe97c621221ed0f280f70f49
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 18:56:56 +0200

    created GlobalContext

---
+++ frontend/src/contexts/GlobalContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import axios from '../utils/axiosClient.js';
+
+const GlobalContext = createContext();
+
+const GlobalProvider = ({ children }) => {
+
+    const [categories, setCategories] = useState([]);
+
+    const fetchCategories = async () => {
+        const res = await axios.get('/categories');
+        const newCategories = res.data;
+        setCategories(newCategories);
+    }
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    return (
+        <GlobalContext.Provider value={categories}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+const useGlobal = () => {
+    const value = useContext(GlobalContext);
+    //se non sono in un consumer del GlobalContext.Provider, value sarà undefined
+    if (value === undefined) {
+        throw new Error('Non sei dentro al Global Provider!');
+    }
+    return value;
+}
+
+export { GlobalProvider, useGlobal }
