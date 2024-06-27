45d372051415cea70693b9602379848173567d8e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 23:43:17 +0200

    refacturing form

--- frontend/src/components/Forms/FormRegister.jsx
+++ frontend/src/components/Forms/FormRegister.jsx
@@ -24,6 +24,7 @@ const FormRegister = ({ submitForm }) => {
             <form onSubmit={handleSubmit} className="card-body">
                 <div className="row">
                     <div className="offset-3 col-6">
+
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email<span className="text-danger"><sup>*</sup></span></label>
                             <input
@@ -37,8 +38,10 @@ const FormRegister = ({ submitForm }) => {
                                 onChange={e => changeData('email', e.target.value)}
                             />
                         </div>
+
                     </div>
                     <div className="offset-3 col-6">
+
                         <div className="mb-3">
                             <label htmlFor="name" className="form-label">Nome</label>
                             <input
@@ -47,12 +50,14 @@ const FormRegister = ({ submitForm }) => {
                                 id="name"
                                 placeholder="Nome"
                                 name="name"
-                                value={formData.name}
+                                value={formData.name || ''}
                                 onChange={e => changeData('name', e.target.value)}
                             />
                         </div>
+
                     </div>
                     <div className="offset-3 col-6">
+
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">Password<span className="text-danger"><sup>*</sup></span></label>
                             <input
@@ -66,6 +71,7 @@ const FormRegister = ({ submitForm }) => {
                                 onChange={e => changeData('password', e.target.value)}
                             />
                         </div>
+
                     </div>
                     <p className="text-center mb-0">I campi contrassegnati con <span className="text-danger"><sup>*</sup></span> sono obbligatori</p>
                 </div>
