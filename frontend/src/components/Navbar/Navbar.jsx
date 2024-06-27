be07959e680563ed8b8aaebc4215de1e8eef3d1e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 18:10:15 +0200

    added logo and colorLogo

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
 import { Link, NavLink } from "react-router-dom";
 import logoImg from "../../assets/img/logo-navbar.jpg";
+import { useAuth } from "../../contexts/AuthContext";
 
 const Navbar = () => {
+
+    const { user, isLogged } = useAuth();
+
+    const logo = () => {
+        const logo = user?.name.split('').filter(l => l !== l.toLowerCase());
+        return logo;
+    }
+
+    const colorLogo = () => {
+        let color = '';
+
+        if (user?.isAdmin && user.isSuperAdmin) color = 'super-admin';
+
+        if (user?.isAdmin && !user.isSuperAdmin) color = 'admin';
+
+        return color;
+    }
+
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -20,26 +39,36 @@ const Navbar = () => {
                                 <li className="nav-item">
                                     <NavLink className="nav-link" to={'/photos'}>Foto</NavLink>
                                 </li>
-                                <li className="nav-item">
+                                {user?.isAdmin && <li className="nav-item">
                                     <a className="nav-link" href="#">Categorie</a>
-                                </li>
-                                <li className="nav-item">
+                                </li>}
+                                {user?.isSuperAdmin && <li className="nav-item">
+                                    <a className="nav-link" href="#">Users</a>
+                                </li>}
+                                {!isLogged && <li className="nav-item">
                                     <a className="nav-link" href="#">Contattaci</a>
-                                </li>
+                                </li>}
                             </ul>
                         </div>
                     </div>
                     <div className="d-flex">
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Login</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Register</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Logout</a>
-                            </li>
+                            {!isLogged && <>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to={'/login'}>Login</NavLink>
+                                </li>
+
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Register</a>
+                                </li>
+                            </>}
+
+                            {isLogged && <li className="nav-item">
+                                <NavLink className="nav-link d-flex gap-2" to={'/dashboard'}>
+                                    <span className={`${colorLogo()} logo`}>{logo()}</span>
+                                    {user.name}
+                                </NavLink>
+                            </li>}
                         </ul>
                     </div>
                 </div>
