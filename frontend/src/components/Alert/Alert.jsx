ad61e4ad315a77f5be1b95327fecf6ce5dd408d7
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 23:42:29 +0200

    fixed alert message

--- frontend/src/components/Alert/Alert.jsx
+++ frontend/src/components/Alert/Alert.jsx
@@ -1,14 +1,14 @@
-const Alert = ({ errors, closeAlert }) => {
+const Alert = ({ generalError, closeAlert }) => {
     return (
         <div className="alert w-50 m-auto mb-4 alert-danger alert-dismissible fade show">
-            {typeof errors === 'array' ?
-                <ul>
-                    {errors.map((err, index) => <li key={`err-${index}`} >{err}</li>)}
-                </ul> :
-                <li>{errors}</li>
-            }
+            <ul>
+                {generalError !== null && <li>{generalError.message}</li>}
+                {generalError?.errors && generalError.errors.map((err, index) => (
+                    <li key={`err-${index}`}>{err.msg}</li>
+                ))}
+            </ul>
             <button onClick={closeAlert} type="button" className="btn-close" aria-label="Close"></button>
-        </div>
+        </div >
     )
 }
 
