dd307da76952484060cc29ca30af38e743a34012
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-28 01:33:15 +0200

    added usercard

--- frontend/src/pages/Auth/Users.jsx
+++ frontend/src/pages/Auth/Users.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from '../../utils/axiosClient.js';
+import UserCard from "../../components/Cards/UserCard.jsx";
+
 const Users = () => {
+
+    const [users, setUsers] = useState();
+
+    const fetchUsers = async () => {
+        const res = await axios.get('/auth/users');
+        setUsers(res.data);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
     return (
         <section className="container">
             <h1 className="text-center">Utenti</h1>
+            <div className="row my-4 g-4">
+                {users?.map((user, index) => (
+                    <div key={`user-${index}`} className="col-3">
+                        <UserCard user={user} />
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
