7a276f5efc8bcd08660040da83fa7969eaa0d080
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 16:23:33 +0200

    added more instance

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -7,4 +7,29 @@ const instance = axios.create({
     timeout: 3000
 });
 
+instance.interceptors.request.use(function (config) {
+
+    const token = localStorage.getItem('accessToken');
+    if (token) config.headers['Authoritazion'] = `Bearer ${token}`;
+
+    return config;
+
+});
+
+instance.interceptors.response.use(
+    function (response) {
+        return response
+    },
+    function (error) {
+        const { status } = error.response;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('user');
+            window.location = "/login";
+        }
+
+        return Promise.reject(error);
+    }
+)
+
 export default instance;
