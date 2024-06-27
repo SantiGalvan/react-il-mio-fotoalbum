77847bc7d40e6a6a83ea111132c2d283e49a1499
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 11:24:50 +0200

    added multer and storage images

--- backend/routers/photos.js
+++ backend/routers/photos.js
@@ -5,9 +5,20 @@ const validator = require('../middlewares/validator.js');
 const { validationSlug } = require("../validations/generalValidation.js");
 const { bodyData } = require("../validations/photos.js");
 const validationToken = require("../middlewares/auth.js");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: "public/photos",
+    filename: (req, file, cf) => {
+        const fileType = path.extname(file.originalname);
+        cf(null, String(Date.now()) + fileType);
+    }
+});
+const upload = multer({ storage });
 
 // Rotta Store
-router.post('/', [validationToken, validator(bodyData)], photosController.store);
+router.post('/', [upload.single("image"), validationToken, validator(bodyData)], photosController.store);
 
 // Rotta Index
 router.get('/', photosController.index);
@@ -19,7 +30,7 @@ router.use('/:slug', validator(validationSlug));
 router.get('/:slug', photosController.show);
 
 // Rotta update
-router.put('/:slug', [validationToken, validator(bodyData)], photosController.update);
+router.put('/:slug', [upload.single("image"), validationToken, validator(bodyData)], photosController.update);
 
 // Rotta Delete
 router.delete('/:slug', validationToken, photosController.destroy);
