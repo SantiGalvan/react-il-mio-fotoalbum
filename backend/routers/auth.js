af2c1bccdd0c678e45b542893a0a50e1c799a1bf
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-27 09:36:39 +0200

    added delete

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const authController = require("../controllers/auth.js");
 const validator = require("../middlewares/validator.js");
 const { registerBody, loginBody } = require("../validations/users.js");
+const validationToken = require("../middlewares/auth.js");
+const authSuperAdmin = require("../middlewares/authSuperAdmin.js");
 
 // Rotta Register
 router.post('/register', validator(registerBody), authController.register);
@@ -10,7 +12,13 @@ router.post('/register', validator(registerBody), authController.register);
 // Rotta Login
 router.post('/login', validator(loginBody), authController.login);
 
+// Validatore del Token e del Super Admin
+router.use('/users', [validationToken, authSuperAdmin])
+
 // Rotta Index
 router.get('/users', authController.index);
+
+// Rotta Delete
+router.delete('/users/:email', authController.destroy);
 
 module.exports = router;
