1c03aa9ac9fe97173d833123fe59ba75a77bc734
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-28 12:18:21 +0200

    imported dashboardcard and added modal component

--- frontend/src/pages/Auth/Dashboard.jsx
+++ frontend/src/pages/Auth/Dashboard.jsx
@@ -1,23 +1,28 @@
 import { useAuth } from "../../contexts/AuthContext";
+import DashboardCard from "../../components/Cards/DashboardCard";
+import Modal from '../../components/Modal/Modal';
+import { useState } from "react";
 
 const Dashboard = () => {
 
     const { user, logout } = useAuth();
 
+    const [logoutMode, setLogoutMode] = useState();
+
     return (
         <section className="container">
             <h1 className="text-center mb-4">Dashboard</h1>
-            <div className="card p-4">
-                <h3 className="text-center">Benvenuto {user.name}</h3>
-                <div className="row text-center my-4">
-                    <div className="col-3">Foto</div>
-                    <div className="col-3">Categorie</div>
-                    <div className="col-3">Users</div>
-                    <div className="col-3">
-                        <button onClick={logout} className="btn btn-danger">Logout</button>
-                    </div>
-                </div>
-            </div>
+            <DashboardCard user={user} clickLogout={() => setLogoutMode(true)} />
+
+            {logoutMode &&
+                <Modal
+                    isShow={logoutMode}
+                    closeModal={() => setLogoutMode(false)}
+                    title={user?.name}
+                    clickLogout={logout}
+                />
+            }
+
         </section>
     )
 }
