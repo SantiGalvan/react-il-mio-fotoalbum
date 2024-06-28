58ca027027892f99e5d2ce725540b344ca35f313
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-28 09:25:08 +0200

    created PhotoCreate page

---
+++ frontend/src/pages/PhotosPages/PhotoCreate.jsx
@@ -0,0 +1,9 @@
+const PhotoCreate = () => {
+    return (
+        <section className="container">
+            <h1 className="text-center">Crea la tua foto</h1>
+        </section>
+    )
+}
+
+export default PhotoCreate;
