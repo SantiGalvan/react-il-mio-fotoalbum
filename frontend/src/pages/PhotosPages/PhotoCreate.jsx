b462d61edcdc254a43087d5e88a7e389f8921206
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-28 10:01:54 +0200

    added PhotoForm

--- frontend/src/pages/PhotosPages/PhotoCreate.jsx
+++ frontend/src/pages/PhotosPages/PhotoCreate.jsx
@@ -1,7 +1,34 @@
+import { FaArrowLeft as ArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import PhotoForm from "../../components/Forms/PhotoForm";
+import axios from '../../utils/axiosClient.js';
+
 const PhotoCreate = () => {
+
+    const navigate = useNavigate();
+
+    const createPhoto = async (formData) => {
+        const res = await axios.post('photos', formData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        });
+
+        if (res.status < 400) navigate(`/photos/${res.data.slug}`);
+    }
+
     return (
         <section className="container">
-            <h1 className="text-center">Crea la tua foto</h1>
+            <div className="mb-4 d-flex align-items-center justify-content-between">
+                <button
+                    onClick={() => { navigate(-1) }}
+                    className='btn btn-secondary h-75 d-flex align-items-center gap-1'
+                >
+                    <ArrowLeft />Torna indietro
+                </button>
+                <h1>Crea la tua Foto</h1>
+            </div>
+            <PhotoForm onSubmit={createPhoto} />
         </section>
     )
 }
