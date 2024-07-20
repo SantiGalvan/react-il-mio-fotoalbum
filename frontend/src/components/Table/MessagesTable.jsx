968bddba5190fd21c9e9c1aa7edc7078306d68d2
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-07-21 01:05:25 +0200

    added date into table

--- frontend/src/components/Table/MessagesTable.jsx
+++ frontend/src/components/Table/MessagesTable.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
 import { FaRegTrashCan as Trash } from "react-icons/fa6";
-import { BsFillPencilFill as Pencil } from "react-icons/bs";
+import { FaRegEye as FaEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const MessagesTable = ({ messages, onDelete }) => {
+
+    const [abstractMessage, setAbstractMessage] = useState();
+
+    const getAbstractMessage = () => {
+
+        const messageContents = messages?.map(message => message.content);
+        const messageToAbstract = messageContents?.filter(message => message.length > 40);
+
+        if (messageToAbstract) {
+
+            let newContent = [];
+
+            for (let i = 0; i < messageToAbstract.length; i++) {
+                const abstract = messageToAbstract[i].toString().substr(0, 40) + '...';
+                newContent.push(abstract);
+            }
+
+            setAbstractMessage(newContent ? newContent : []);
+        }
+    }
+
+    useEffect(() => {
+        getAbstractMessage();
+    }, [])
+
     return (
+
         <table className="table table-dark">
             <thead>
                 <tr>
@@ -10,25 +38,31 @@ const MessagesTable = ({ messages, onDelete }) => {
                     <th scope="col">Content</th>
                     <th scope="col">User Name</th>
                     <th scope="col">User Email</th>
+                    <th scope="col">Data</th>
                     <th scope="col"></th>
                 </tr>
             </thead>
+
             <tbody>
+
                 {messages?.map((message, index) => (
                     <tr key={`message-${index}`}>
 
                         <th scope="row">{message.email}</th>
 
-                        <td>{message.content}</td>
+                        {abstractMessage && <td>{message.content.length > 40 ? abstractMessage[index] : message.content}</td>}
 
                         <td>{message.user.name}</td>
 
                         <td>{message.user.email}</td>
 
+                        <td>{message.createdAt}</td>
+
                         <td>
 
                             <div className='d-flex gap-2'>
 
+                                <Link to={`/messages/${message.id}`} className='btn btn-sm btn-primary d-flex align-items-center justify-content-center'><FaEye /></Link>
                                 <button onClick={() => { onDelete(message.id) }} className='btn btn-sm btn-danger d-flex align-items-center justify-content-center'><Trash /></button>
 
                             </div>
@@ -37,7 +71,9 @@ const MessagesTable = ({ messages, onDelete }) => {
 
                     </tr>
                 ))}
+
             </tbody>
+
         </table>
     )
 }
