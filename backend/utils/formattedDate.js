1d5eb9e0181606c91fa3fe94d5171f85ec22175f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-07-21 01:04:36 +0200

    created funcion formattedDate

---
+++ backend/utils/formattedDate.js
@@ -0,0 +1,17 @@
+
+const formattedDate = (dateToFormatted) => {
+    const date = new Date(dateToFormatted);
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    const italianDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+    return italianDate;
+}
+
+module.exports = formattedDate;
