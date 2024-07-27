cd6dd84812d634385023634c44518c91f13794fc
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-07-27 14:06:41 +0200

    refactoring

--- frontend/src/layouts/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout.jsx
@@ -1,13 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import { useDarkMode } from "../contexts/DarkModeContext";
 
 const DefaultLayout = () => {
-    return (<>
-        <Navbar />
-        <main>
-            <Outlet />
-        </main>
-    </>)
+
+    const { isDark } = useDarkMode();
+
+    return (
+        <div className="App" data-theme={isDark ? 'dark' : 'light'}>
+
+            <Navbar />
+
+            <main>
+                <Outlet />
+            </main>
+
+        </div>
+    )
 }
 
 export default DefaultLayout;
