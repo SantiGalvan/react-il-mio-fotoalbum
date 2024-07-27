ed02d9e90e341c4bb58cf3917dde13f1cf21e392
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-07-27 21:10:43 +0200

    fixed button

--- frontend/src/pages/PhotosPages/PhotoShow.jsx
+++ frontend/src/pages/PhotosPages/PhotoShow.jsx
@@ -24,7 +24,7 @@ const PhotoShow = () => {
         const res = await axios.get(`/photos/${slug}`);
         const newPhoto = res.data;
         setPhoto(newPhoto);
-        setUserMessage(curr => ({ ...curr, name: newPhoto.user.name, id: newPhoto.userId, photoId: slug }));
+        setUserMessage(curr => ({ ...curr, name: newPhoto.user.name, id: newPhoto.userId, photoId: newPhoto.id }));
     }
 
     const deletePhoto = async () => {
@@ -43,7 +43,7 @@ const PhotoShow = () => {
         <section className="container">
 
             {photo && <> <button
-                onClick={() => { navigate('/photos') }}
+                onClick={() => { navigate(-1) }}
                 className='btn btn-secondary h-75 d-flex align-items-center gap-1 mb-4'
             >
                 <ArrowLeft />Torna indietro
