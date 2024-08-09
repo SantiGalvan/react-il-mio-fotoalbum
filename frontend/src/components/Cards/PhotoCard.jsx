36485483a780b27d143fed5a8d5f35e7f47596f5
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-08-09 11:43:50 +0200

    added validated card style and added new button validat

--- frontend/src/components/Cards/PhotoCard.jsx
+++ frontend/src/components/Cards/PhotoCard.jsx
@@ -3,9 +3,10 @@ import photoCardStyle from './PhotoCard.module.scss';
 import { BsFillPencilFill as Pencil } from "react-icons/bs";
 import { FaRegTrashCan as Trash } from "react-icons/fa6";
 import { useAuth } from '../../contexts/AuthContext';
+import { GrValidate } from "react-icons/gr";
 import polaroid from '../../assets/img/polaroid.png'
 
-const PhotoCard = ({ title, author, description, image, categories, visible, slug, isShow, onDelete }) => {
+const PhotoCard = ({ title, author, description, image, categories, visible, slug, isShow, onDelete, validated }) => {
 
     const { user } = useAuth();
 
@@ -20,7 +21,7 @@ const PhotoCard = ({ title, author, description, image, categories, visible, slu
     }
 
     return (
-        <div onClick={goShow} className={`${isShow ? photoCardStyle.showCard : photoCardStyle.indexCard} ${visible ? '' : photoCardStyle.invisible}`}>
+        <div onClick={goShow} className={`${isShow ? photoCardStyle.showCard : photoCardStyle.indexCard} ${visible ? '' : photoCardStyle.invisible} ${validated ? '' : photoCardStyle.invalid}`}>
 
             <figure>
                 <img src={`${image}`} alt={title} />
@@ -57,6 +58,14 @@ const PhotoCard = ({ title, author, description, image, categories, visible, slu
                         {(author?.name == user?.name || user?.isSuperAdmin) &&
                             <>
                                 <Link to={`/photos/${slug}/edit`} className={`${photoCardStyle.dataBtn} btn btn-warning d-flex align-items-center gap-1 my-4`}><Pencil />Modifica</Link>
+
+                                {(user.isSuperAdmin && !validated) &&
+                                    <button
+                                        className={`${photoCardStyle.dataBtn} btn btn-success d-flex align-items-center gap-1 my-4`}>
+                                        <GrValidate />Valida
+                                    </button>
+                                }
+
                                 <button
                                     onClick={onDelete}
                                     className={`${photoCardStyle.dataBtn} btn btn-danger d-flex align-items-center gap-1 my-4`}>
@@ -64,6 +73,7 @@ const PhotoCard = ({ title, author, description, image, categories, visible, slu
                                 </button>
                             </>
                         }
+
                     </div>
 
                 }
